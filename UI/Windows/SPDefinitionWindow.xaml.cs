c1b6aa9e589752b01b20c9a02e79448d3f549476
TheJelle <Julien.Kluge@gmail.com>
2015-03-26 12:13:42 +0100

    Revert "Revert "Improved SPDef Window""
    This reverts commit b346096b415db30d7ff0b77235ee04b541d4f4bf.

--- UI/Windows/SPDefinitionWindow.xaml.cs
+++ UI/Windows/SPDefinitionWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MahApps.Metro.Controls;
 using Spedit.SPCondenser;
 using System;
+using System.Collections.Generic;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -14,6 +16,10 @@ namespace Spedit.UI.Windows
     {
         CondensedSourcepawnDefinition def;
 
+        SPDefEntry[] defArray;
+        ListViewItem[] items;
+        Timer searchTimer = new Timer(1000.0);
+
         public SPDefinitionWindow()
         {
             InitializeComponent();
@@ -24,56 +30,101 @@ namespace Spedit.UI.Windows
                 this.Close();
                 return;
             }
-            for (int i = 0; i < def.Functions.Length; ++i)
+            List<SPDefEntry> defList = new List<SPDefEntry>();
+            for (int i = 0; i < def.Functions.Length; ++i) { defList.Add((SPDefEntry)def.Functions[i]); }
+            for (int i = 0; i < def.Constants.Length; ++i) { defList.Add(new SPDefEntry() { Name = def.Constants[i], Entry = "Constant" }); }
+            for (int i = 0; i < def.Types.Length; ++i) { defList.Add(new SPDefEntry() { Name = def.Types[i], Entry = "Type" }); }
+            for (int i = 0; i < def.MethodNames.Length; ++i) { defList.Add(new SPDefEntry() { Name = def.MethodNames[i], Entry = "Method" }); }
+            for (int i = 0; i < def.Properties.Length; ++i) { defList.Add(new SPDefEntry() { Name = def.Properties[i], Entry = "Property" }); }
+            defList.Sort((a, b) => { return string.Compare(a.Name, b.Name); });
+            defArray = defList.ToArray();
+            int defArrayLength = defArray.Length;
+            items = new ListViewItem[defArrayLength];
+            for (int i = 0; i < defArrayLength; ++i)
             {
-                SPFunctionsListBox.Items.Add(def.Functions[i].Name);
+                items[i] = new ListViewItem() { Content = defArray[i].Name, Tag = defArray[i].Entry };
+                SPBox.Items.Add(items[i]);
             }
-            for (int i = 0; i < def.Constants.Length; ++i)
-            {
-                ConstantsList.Items.Add(def.Constants[i]);
-            }
-            for (int i = 0; i < def.Types.Length; ++i)
-            {
-                TypesList.Items.Add(def.Types[i]);
-            }
-            for (int i = 0; i < def.MethodNames.Length; ++i)
-            {
-                MethodsList.Items.Add(def.MethodNames[i]);
-            }
-            if (SPFunctionsListBox.Items.Count > 0)
-            {
-                SPFunctionsListBox.SelectedIndex = 0;
-            }
+            searchTimer.Elapsed += searchTimer_Elapsed;
+        }
+
+        void searchTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DoSearch();
         }
 
         private void SPFunctionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            SPFunctionNameBlock.Text = def.Functions[SPFunctionsListBox.SelectedIndex].Name;
-            SPFunctionFullNameBlock.Text = def.Functions[SPFunctionsListBox.SelectedIndex].FullName;
-            SPFunctionCommentBox.Text = def.Functions[SPFunctionsListBox.SelectedIndex].Comment;
+            object obj = SPBox.SelectedItem;
+            if (obj == null) { return; }
+            ListViewItem item = (ListViewItem)obj;
+            object TagValue = item.Tag;
+            if (TagValue != null)
+            {
+                if (TagValue is SPFunction)
+                {
+                    SPFunction func = (SPFunction)TagValue;
+                    SPNameBlock.Text = func.Name;
+                    SPFullNameBlock.Text = func.FullName;
+                    SPCommentBox.Text = func.Comment;
+                    return;
+                }
+                else if (TagValue is string)
+                {
+                    SPNameBlock.Text = (string)item.Content;
+                    SPFullNameBlock.Text = (string)TagValue;
+                    SPCommentBox.Text = string.Empty;
+                    return;
+                }
+            }
+            SPNameBlock.Text = (string)item.Content;
+            SPFullNameBlock.Text = string.Empty;
+            SPCommentBox.Text = string.Empty;
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string searchString = ((TextBox)sender).Text;
-            bool foundOccurence = false;
-            for (int i = 0; i < def.Functions.Length; ++i)
-            {
-                if (def.Functions[i].Name.StartsWith(searchString, StringComparison.InvariantCultureIgnoreCase))
+            SPProgress.IsIndeterminate = true;
+            searchTimer.Stop();
+            searchTimer.Start();
+        }
+
+        private void DoSearch()
+        {
+            this.Dispatcher.Invoke(() =>
                 {
-                    SPFunctionsListBox.SelectedIndex = i;
-                    SPFunctionsListBox.ScrollIntoView(SPFunctionsListBox.SelectedItem);
-                    foundOccurence = true;
-                    break;
-                }
-            }
-            if (foundOccurence)
+                    int itemCount = defArray.Length;
+                    string searchString = SPSearchBox.Text.ToLowerInvariant();
+                    bool foundOccurence = false;
+                    SPBox.Items.Clear();
+                    for (int i = 0; i < itemCount; ++i)
+                    {
+                        if (defArray[i].Name.ToLowerInvariant().Contains(searchString))
+                        {
+                            foundOccurence = true;
+                            SPBox.Items.Add(items[i]);
+                        }
+                    }
+                    if (foundOccurence)
+                    {
+                        SPSearchBox.Background = Brushes.White;
+                    }
+                    else
+                    {
+                        SPSearchBox.Background = Brushes.LightYellow;
+                    }
+                    SPProgress.IsIndeterminate = false;
+                });
+        }
+
+        private class SPDefEntry
+        {
+            public string Name;
+            public object Entry;
+
+            public static explicit operator SPDefEntry(SPFunction func)
             {
-                ((TextBox)sender).Background = null;
-            }
-            else
-            {
-                ((TextBox)sender).Background = Brushes.LightYellow;
+                return new SPDefEntry() { Name = func.Name, Entry = func };
             }
         }
     }
