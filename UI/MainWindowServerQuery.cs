b22ff6f979aaeedeadb7a8cd1615911d0aee281a
TheJelle <Julien.Kluge@gmail.com>
2015-05-03 15:05:01 +0200

    Sending Multiple RCON Querys with one Cmd
    Sending Commands delimited with a semicolon to gain speed, reduce lags
    and keep log files cleaner.

--- UI/MainWindowServerQuery.cs
+++ UI/MainWindowServerQuery.cs
@@ -64,7 +64,7 @@ namespace Spedit.UI
                 replacement.AppendLine();
                 for (int i = 0; i < compiledFileNames.Count; ++i)
                 {
-                    replacement.AppendLine("sm plugins reload " + compiledFileNames[i]);
+                    replacement.Append("sm plugins reload " + compiledFileNames[i] + ";");
                 }
                 replacement.AppendLine();
                 input = input.Replace("{plugins_reload}", replacement.ToString());
@@ -75,7 +75,7 @@ namespace Spedit.UI
                 replacement.AppendLine();
                 for (int i = 0; i < compiledFileNames.Count; ++i)
                 {
-                    replacement.AppendLine("sm plugins load " + compiledFileNames[i]);
+                    replacement.Append("sm plugins load " + compiledFileNames[i] + ";");
                 }
                 replacement.AppendLine();
                 input = input.Replace("{plugins_load}", replacement.ToString());
@@ -86,7 +86,7 @@ namespace Spedit.UI
                 replacement.AppendLine();
                 for (int i = 0; i < compiledFileNames.Count; ++i)
                 {
-                    replacement.AppendLine("sm plugins unload " + compiledFileNames[i]);
+                    replacement.Append("sm plugins unload " + compiledFileNames[i] + ";");
                 }
                 replacement.AppendLine();
                 input = input.Replace("{plugins_unload}", replacement.ToString());
