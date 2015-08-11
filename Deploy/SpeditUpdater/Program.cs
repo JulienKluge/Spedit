8cb4956851f05dc21f554f863eef273f8dd14281
TheJelle <Julien.Kluge@gmail.com>
2015-08-11 22:16:11 +0200

    Revert "Revert "Additionally add files from 1.0.3.1 release""
    This reverts commit 03d8a0fe5187fa36d67230540b1d2e9090bbb91b.

--- Deploy/SpeditUpdater/Program.cs
+++ Deploy/SpeditUpdater/Program.cs
@@ -44,7 +44,7 @@ namespace SpeditUpdater
             UpdateMarquee um = (UpdateMarquee)arg;
             string zipFile = Path.Combine(Environment.CurrentDirectory, "updateZipFile.zip");
 
-            byte[] zipFileContent = SpeditUpdater.Properties.Resources.spedit1_0_3_0Update;
+            byte[] zipFileContent = SpeditUpdater.Properties.Resources.spedit1_0_3_1Update;
 
             File.WriteAllBytes(zipFile, zipFileContent);
 
