71acf2fc6bd80a1cb67ff0965f3e5c3914fcc8b7
Shahriyar Bazaei <shahriyar.ba@icloud.com>
2017-04-07 20:53:54 +0430

    Cleanup SpeditUpdater

--- Deploy/SpeditUpdater/UpdateMarquee.cs
+++ Deploy/SpeditUpdater/UpdateMarquee.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SpeditUpdater
@@ -15,21 +8,21 @@ namespace SpeditUpdater
         public UpdateMarquee()
         {
             InitializeComponent();
-            Bitmap bmp = SpeditUpdater.Properties.Resources.IconPng;
-            pictureBox1.Image = (Image)bmp;
+
+            pictureBox1.Image = Properties.Resources.IconPng;
         }
 
         public void SetToReadyState()
         {
-            label1.Text = "SPEdit got updated!";
+            label1.Text = @"SPEdit got updated!";
             progressBar1.Visible = false;
             button1.Visible = true;
-            this.UseWaitCursor = false;
+            UseWaitCursor = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
     }
 }
